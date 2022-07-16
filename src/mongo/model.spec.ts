daee58da9b4decc35274e5b7b7590009cf90969d
christian-theriot <christian@theriot.dev>
2022-07-16 11:33:21 -0400

    Remove mongo connection

--- src/mongo/model.spec.ts
+++ src/mongo/model.spec.ts
@@ -1,25 +1,8 @@
-import { Database, ModelFactory } from '.';
-import env from '@theriot.dev/env';
+import { ModelFactory } from '.';
 import mongoose from 'mongoose';
 
 describe('Mongo Model Factory', () => {
-  let db: Database;
   let Model: any;
-
-  beforeAll(async () => {
-    const user = env.get('MONGO_USER');
-    const pwd = env.get('MONGO_PASSWORD');
-    const host = env.get('MONGO_HOST');
-    const port = env.get('MONGO_PORT');
-    const dbName = env.get('MONGO_DB');
-
-    db = new Database(`mongodb://${user}:${pwd}@${host}:${port}/${dbName}`);
-    await db.start();
-  });
-
-  afterAll(async () => {
-    await db.stop();
-  });
 
   it('can create a model using the factory', async () => {
     jest
